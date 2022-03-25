f44142cb710c75bd27e0cced2e0dfd04f6a2a6f4
Andrius B <tyroxy@gmail.com>
2022-03-25 11:02:46 +0200

    submit for review

--- app.js
+++ app.js
@@ -28,7 +28,73 @@ Prieš patalpinant vaisių į grauztukai objektą, vaisius turi būti
 */
 
 
+const rand = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+class Vaisius {
+
+    constructor() {
+        this.dydis = this.rand(5, 25);
+        this.id = this.rand(1000000, 9999999)
+        this.prakastas = false;
+    }
+
+    prakasti(vaisius) {
+        if (vaisius.prakastas) {
+            this.prakastas = true;
+        }
+        
+    }
+
+    rand(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
 
 
+
+class Krepsys {
+
+    static vaisiai = [];
+
+    static pripildyti(){
+         
+        if (this.vaisiai.length === 0) {
+            for (let i = 0; i < 20; i++) {
+             this.vaisiai.push(new Vaisius());
+        }
+            this.vaisiai.sort((a, b) => b.dydis - a.dydis);
+        } else {
+            this.papildyti();
+      }
+    }
+
+    static isimti() {
+        const did = this.vaisiai[0];
+        this.vaisiai.shift();
+
+        did.prakasti();
+        return did;
+
+      }
+    
+    static papildyti() {
+        while (this.vaisiai.length < 20) {
+            this.vaisiai.push(new Vaisius());
+        }
+    }
+
+}
+
+Krepsys.pripildyti();
+
+console.log(new Vaisius());
+
+console.log(Krepsys.vaisiai);
 
 
