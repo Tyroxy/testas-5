0b7c973c494ad556020312634aec627472d19752
Andrius B <tyroxy@gmail.com>
2022-03-25 09:35:04 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,34 @@
+/*
+
+TTarpinis OOP uždavinys
+
+Sukurti klasę Vaisius, kuris turi:
+savybę dydis rand 5 - 25;
+savybę id rand 1000000 - 9999999
+savybę prakastas false
+Sukurti metodą prakasti(), kuris savybe prakastas keistų į true.
+Sukurti klasę Krepšys, kuri turi statinę savybe vaisiai, kuri
+yra masyvas.
+Klasėje Krepšys sukurkite statinį metodą pripildyti(),
+kuris savybę vaisiai užpildytų 20-timi Vaisius objektų ir
+išrūšiuotų juos pagal vaisiaus dydį nuo didžiausio iki
+mažiausio.
+Klasėje Krepšys sukurti statinį metodą isimti(), kuris iš
+vaisiai masyvo išimtų (ištrintų iš masyvo) pirmą (didžiausią)
+vaisių ir jį grąžintų. Išėmus tarkim kelis vaisius ir vėl
+paleidus metodą papildyti(), jis turi padaryti tai ką sako
+metodo pavadinimas- papildyti iki pilno (20 elementų), o ne
+perrašyti visus vaisius iš naujo (tai galima stebėti pagal
+vaisių id).
+Išorėje (globale) sukurti kintamąjį grauztukai kuris yra Map
+tipo objektas. Iš krepšelio išimti vaisiai turi būti pridedami į
+šį objektą. Kaip raiktus naudoti Vaisius objekto id savybę.
+Prieš patalpinant vaisių į grauztukai objektą, vaisius turi būti
+“prakąstas”, Vaisius objekte paleidžiant prakasti() metodą.
+*/
+
+
+
+
+
+
